414bc11c15e8277e10495ad7f894c36a2652ca45
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2018-10-07 21:19:45 -0700

    updated to be set in heroku

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -3,9 +3,11 @@
 const awsSDKMock = require('aws-sdk-mock');
 const faker = require('faker');
 
-process.env.PORT = 4000;
-process.env.MONGODB_URI = 'mongodb://localhost/testdb';
-process.env.APP_SECRET = 'CRd25f3JJn7eU27aE57Y7ub6hatF3FKvVSa8t42835j8S67CNTyG4vF24aiSbMkLZ6jVWt';
+// added these via heroku on this branch specifically
+
+// process.env.PORT = 4000;
+// process.env.MONGODB_URI = 'mongodb://localhost/testdb';
+// process.env.APP_SECRET = 'CRd25f3JJn7eU27aE57Y7ub6hatF3FKvVSa8t42835j8S67CNTyG4vF24aiSbMkLZ6jVWt';
 
 // added these via heroku on this branch specifically
 // process.env.AWS_ACCESS_KEY_ID = 'SECRET ID';
